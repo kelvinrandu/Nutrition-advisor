7680f3ee682572aeb0275f304674382a71845427
kelvinrandu <randukelvin@gmail.com>
2019-11-18 23:35:21 +0300

    added second level ussd code

--- ussdap.php
+++ ussdap.php
@@ -7,37 +7,49 @@ $text        = $_POST["text"];
 
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
-    $response  = "CON What would you want to check \n";
-    $response .= "1. My Account \n";
-    $response .= "2. My phone number";
+     $response  = "CON .....Afyaplus..... \n";
+    $response  .= "Chagua lugha\n";
+    $response .= "1. Endelea na Kiswahili \n";
+    $response .= "2. Enderera na chidigo \n";
+    $response .= "3. Enderera na chiduruma \n";
+    $response .= "4. Continue in English";
 
 } else if ($text == "1") {
-    // Business logic for first level response
-    $response = "CON Choose account information you want to view \n";
-    $response .= "1. Account number \n";
-    $response .= "2. Account balance";
+// level 2 Kiswahili
+    $response  = "CON Unyafuzi  \n";
+    $response .= "1. Unyafuzi ni nini?\n";
+    $response .= "2. Dalili kuu za Unyafuzi. \n";
+    $response .= "3. Aina za Unyafuzi \n";
+    $response .= "4. Rudi nyuma \n";
+    $response .= "5. Toka";
 
 } else if ($text == "2") {
-    // Business logic for first level response
-    // This is a terminal request. Note how we start the response with END
-    $response = "END Your phone number is ".$phoneNumber;
+// level 2 Digo
+    $response  = "CON .....Afyaplus..... \n";
+    $response  .= "Chagua lugha\n";
+    $response .= "1. Endelea na Kiswahili \n";
+    $response .= "2. Enderera na chidigo \n";
+    $response .= "3. Enderera na chiduruma\n";
+    $response .= "4. Continue in English";
 
-} else if($text == "1*1") { 
-    // This is a second level response where the user selected 1 in the first instance
-    $accountNumber  = "ACC1001";
-
-    // This is a terminal request. Note how we start the response with END
-    $response = "END Your account number is ".$accountNumber;
-
-} else if ( $text == "1*2" ) {
-    // This is a second level response where the user selected 1 in the first instance
-    $balance  = "KES 10,000";
-
-    // This is a terminal request. Note how we start the response with END
-    $response = "END Your balance is ".$balance;
+} else if($text == "3") { 
+// level 2 Kiduruma
+    $response  = "CON .....Afyaplus..... \n";
+    $response  .= "Chagua lugha\n";
+    $response .= "1. Endelea na Kiswahili \n";
+    $response .= "2. Enderera na chidigo \n";
+    $response .= "3. Enderera na chiduruma \n";
+    $response .= "4. Continue in English";
+} else if ( $text == "4" ) {
+// level 2 english
+    $response  = "CON P.E.M \n";
+    $response .= "1. What is P.E.M \n";
+    $response .= "2. Common symptoms of P.E.M \n";
+    $response .= "3. Types of P.E.M \n";
+    $response .= "4. Back\n";
+    $response .= "5. Exit";
 }
 
 // Echo the response back to the API
 header('Content-type: text/plain');
 echo $response;
-USSD
