01b89f37a7a530205cf6bc8e610adadb427bac86
kelvinrandu <randukelvin@gmail.com>
2019-12-10 12:53:19 +0300

    added level 3 english

--- ussdap.php
+++ ussdap.php
@@ -48,6 +48,23 @@ if ($text == "") {
     $response .= "3. Types of P.E.M \n";
     $response .= "4. Back\n";
     $response .= "5. Exit";
+} else if ( $text == "4*1" ) {
+    // level 3 english
+    $response = "END Protein–energy malnutrition (PEM) is a form of malnutrition that is defined as a range of pathological conditions arising from coincident lack of dietary protein and/or energy (calories) in varying proportions.  ";
+}else if ( $text == "4*2" ) {
+    // level 3 english
+    $response  = "CON Common symptoms of P.E.M \n";
+    $response .= "1. What is P.E.M \n";
+    $response .= "2. Common symptoms of P.E.M \n";
+    $response .= "3. Types of P.E.M \n";
+    $response .= "4. Back\n";
+    $response .= "5. Exit";
+}else if ( $text == "4*2" ) {
+    // level 3 english common symptoms of P.E.M
+    $response = "END Protein–energy malnutrition (PEM) is a form of malnutrition that is defined as a range of pathological conditions arising from coincident lack of dietary protein and/or energy (calories) in varying proportions.  ";
+}else if ( $text == "4*3" ) {
+    // level 3 english types of P.E.M
+    $response = "END Protein–energy malnutrition (PEM) is a form of malnutrition that is defined as a range of pathological conditions arising from coincident lack of dietary protein and/or energy (calories) in varying proportions.  ";
 }
 
 // Echo the response back to the API
